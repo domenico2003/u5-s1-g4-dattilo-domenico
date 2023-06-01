2e8a04f937e806bd20af1774e17ba4ccdc99c4d9
domenico2003 <domdat03@gmail.com>
2023-06-01 15:00:15 +0200

    first commit

---
+++ src/main/java/Application/EpicpizzaApplication.java
@@ -0,0 +1,13 @@
+package Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicpizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicpizzaApplication.class, args);
+	}
+
+}
